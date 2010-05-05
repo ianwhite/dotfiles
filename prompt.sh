9107978c35e5c74e63a9c7c41f6ca0a2882bf851
Ian White <ian.w.white@gmail.com>
2010-05-05 08:58:49 +0100

    Enclose non-printing cvhars in \[ \] to stop word wrap issue in bash

--- prompt.sh
+++ prompt.sh
@@ -1,11 +1,11 @@
-black='\e[0;30m'
-red='\e[0;31m'
-green='\e[0;32m'
-blue='\e[0;34m'
-purple='\e[0;35m'
-cyan='\e[0;36m'
-grey='\e[0;37m'
-normal='\e[m'
+black='\[\e[0;30m\]'
+red='\[\e[0;31m\]'
+green='\[\e[0;32m\]'
+blue='\[\e[0;34m\]'
+purple='\[\e[0;35m\]'
+cyan='\[\e[0;36m\]'
+grey='\[\e[0;37m\]'
+normal='\[\e[m\]'
 
 user="$green\u@\h$normal" 
 dir=" $blue\W$normal"
