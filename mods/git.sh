641f3bee8dcbf5c0abba6f57e6f40dd8db0f8af6
Ian White <ian.w.white@gmail.com>
2010-08-18 12:51:54 +0100

    touch .no-git-dirty in large projects to save speed on PS1

--- mods/git.sh
+++ mods/git.sh
@@ -4,11 +4,10 @@ source /usr/local/etc/bash_completion.d/git-completion.bash
 # functions
 
 # Git "dirty?" - for use in prompt
+#
+# touch .no-git-dirty in your projects root to turn this off
 git-dirty() {
-	# ignore large git repos that I know about
-	if [[ $PWD =~ db/dump.* ]]; then
-		echo "?"
-	else
+	if [[ ! -e "$(git rev-parse --show-cdup 2>/dev/null || echo './').no-git-dirty" ]]; then
 		git status 2>/dev/null | wc -l | awk '{if ($1 > 2) print "☆"}'
 	fi
 }
